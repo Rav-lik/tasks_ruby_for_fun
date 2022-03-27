b16c0a4394027d48da3b97c1653db03f89abc32f
Andrey Moisseyev <ravl_kz@mail.ru>
2022-03-27 23:44:21 +0600

    Added 1-st app.rb

---
+++ app.rb
@@ -0,0 +1,32 @@
+puts "What is your year of birth?"
+birth_year_1 = gets
+puts "What is your place of birth?"
+birth_place_1 = gets
+puts "What is your phone number?"
+phone_1 = gets
+puts "What is your year of birth?"
+birth_year_2 = gets
+puts "What is your place of birth?"
+birth_place_2 = gets
+puts "What is your phone number?"
+phone_2 = gets
+puts "What is your year of birth?"
+birth_year_3 = gets
+puts "What is your place of birth?"
+birth_place_3 = gets
+puts "What is your phone number?"
+phone_3 = gets
+puts "First client:"
+puts "	Year of the birth - " + birth_year_1
+puts "	Place of birth - " + birth_place_1
+puts "	Phone number - " + phone_1
+puts
+puts "Second client:"
+puts "	Year of the birth - " + birth_year_2
+puts "	Place of birth - " + birth_place_2
+puts "	Phone number - " + phone_2
+puts
+puts "Third client:"
+puts "	Year of the birth - " + birth_year_3
+puts "	Place of birth - " + birth_place_3
+puts "	Phone number - " + phone_3
